61e11012b6fb8cc5537b34458515f25b4ab793eb
JCPB2000 <jcpaguay1@espe.edu.ec>
2025-05-18 18:51:36 -0500

    Implemetacion de if else y funcion , prompt para introducir edad

---
+++ public/js/edadCategoria.js
@@ -0,0 +1,16 @@
+function clasificarEdad() {
+  let edad = prompt("Introduce tu edad:");
+  edad = Number(edad);
+
+  if (edad < 0) {
+    console.log("Edad no válida");
+  } else if (edad <= 12) {
+    console.log("Niño");
+  } else if (edad <= 17) {
+    console.log("Adolescente");
+  } else if (edad <= 59) {
+    console.log("Adulto");
+  } else {
+    console.log("Adulto mayor");
+  }
+}
